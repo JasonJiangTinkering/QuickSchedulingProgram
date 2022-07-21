c31910a52129448546bd8a40230401eab2faaa3b
“jasonjiangstuy” <jasonjiangny@gmail.com>
2022-07-21 01:46:17 -0400

    created game plan, questions + finsihed unoptimized example

---
+++ main.py
@@ -0,0 +1,9 @@
+from ortools.sat.python import cp_model
+
+# criteria
+#SYEP participant can work 25 hours a week
+#Participants get 1 lunch break for at least 30 mins after 6 hours 11:00am - 2:00pm
+#at Comik ink, its 1 hour from 3:00 - 4:00
+#SYEP particpants work for 6 weeks
+
+
---
+++ questions.md
@@ -0,0 +1,5 @@
+What do people put in their prefrences?
+
+Whats the best way to store this data?
+
+Whats the best way to create a future proof data entry UI
